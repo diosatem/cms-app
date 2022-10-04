a57294f6a9e6f01bfdb2d38523764f5d3bb841d9
Use your GitHub username <jam19021@byui.edu>
2022-10-05 04:14:55 +0800

    changes

--- src/app/messages/message-list/message-edit/message-edit.component.ts
+++ src/app/messages/message-list/message-edit/message-edit.component.ts
@@ -9,12 +9,11 @@ import { Message } from '../message.model';
 })
 
 export class MessageEditComponent implements OnInit {
-  currentSender: string = 'Diosa';
-  // @ViewChild('id', {static: false}) idRef: ElementRef;
+  currentSender: string = 'Diosa';  
+  @Output() addMessageEvent = new EventEmitter<Message>();
   @ViewChild('subject', { static: false }) subject: ElementRef;
   @ViewChild('msgText', { static: false }) msgText: ElementRef;
   @ViewChild('sender', { static: false }) sender: ElementRef;
-  @Output() addMessageEvent = new EventEmitter<Message>();
 
   constructor() { }
 
@@ -22,19 +21,20 @@ export class MessageEditComponent implements OnInit {
   }
 
   onSendMessage() {
+    console.log("Sent");
     const subj = this.subject.nativeElement.value;
     const msgTxt = this.msgText.nativeElement.value;
     const num = 1;
-    const currSender = this.currentSender; 
+    const currSender = this.currentSender;
     const newMsg = new Message(num, subj, msgTxt, currSender);
-    console.log('Send')
     this.addMessageEvent.emit(newMsg);
   }
 
   onClear() {
-    // let subj = this.subjectRef.nativeElement.value;
-    // let msgTxt = this.msgTextRef.nativeElement.value;
-
-    console.log("Cleared")
+    let subj = this.subject.nativeElement.value;
+    let msgTxt = this.msgText.nativeElement.value;
+    subj = "";
+    msgTxt = "";
+    console.log("Cleared");
   }
 }
